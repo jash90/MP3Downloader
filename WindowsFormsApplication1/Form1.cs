e96650088dd685e040b0a6fa03bf1405171430c0
jash90 <bartekziimny90@gmail.com>
2015-02-19 23:40:16 +0100

    MP3

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string p = "http://mp3.teledyski.info/index.html?text=";
+        string k = "&file=global_test&name=eplik&&dalej=Szukaj";
+        List<ListBox> listbox = new List<ListBox>();
+        List<List<String>> linki = new List<List<string>>();
+        List<int> index = new List<int>();
+        List<int> imax = new List<int>();
+        int indexlist = -1;
+        private void button1_Click(object sender, EventArgs e)
+        {
+           indexlist++;
+           TabPage tabpage1 = new TabPage();
+           tabpage1.Text = textBox1.Text;
+           tabControl1.TabPages.Add(tabpage1);
+           listbox.Add(new ListBox());
+           listbox[indexlist].Dock = DockStyle.Fill;
+           listbox[indexlist].ScrollAlwaysVisible = true;
+           tabpage1.Controls.Add(listbox[indexlist]);
+           linki.Add(new List<String>());
+           index.Add(new int());
+           index[index.Count - 1]=0;
+           tabControl1.SelectedTab = tabpage1;
+           listbox[indexlist].SelectionMode = SelectionMode.One;
+           this.listbox[indexlist].Click += new System.EventHandler(this.listbox_Click);
+           tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
+           Wyszukaniestrony("");
+           
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            TabControl t = (TabControl)sender;
+            indexlist = t.SelectedIndex;
+            label1.Text = "Ilość piosenek wynosi = " + listbox[indexlist].Items.Count.ToString();
+        }
+
+
+        public void Wyszukaniestrony(String s)
+        {
+            try
+            {
+                
+                WebClient w = new WebClient();
+                string page = w.DownloadString(p + zamiananaHTML() + k + s);
+                string name = "href=\"http://h+(.*?)\">+(.*?).mp3";
+                foreach (Match match in Regex.Matches(page, name))
+                {
+                    String link ="http://www.h" + match.Groups[1].Value;
+                    String nazwa =match.Groups[2].Value.Replace("<b style='color:black;background-color:#FFCC66'>", "").ToString().Replace("</b>", "").ToString();
+                    listbox[indexlist].Items.Add(nazwa);
+                    linki[indexlist].Add(link);
+                }
+                string name2 = "href=\"index.html+(.*?)\">+(.*?)";
+                int max = 0;
+                foreach (Match match2 in Regex.Matches(page, name2))
+                {
+                    String link = match2.Groups[1].Value;
+                    if (link.Contains("&amp;file=global_test&amp;name=eplik&amp;dalej=Szukaj&amp;strona=") == true)
+                    {
+                        int pos = link.LastIndexOf("=");
+                        link = link.Substring(pos+1);
+                        if (int.Parse(link) > max)
+                        {
+                            max = int.Parse(link);
+                        }
+                       
+                    }
+                }
+                imax.Add(max);
+                toolStripStatusLabel1.Text = index[indexlist]+1.ToString() + " / " + imax[indexlist]+1.ToString();
+                button4.Visible=true;
+                label1.Text = "Ilość piosenek wynosi = " + listbox[indexlist].Items.Count.ToString();
+                if (index[tabControl1.SelectedIndex] > 0)
+                {
+                    label2.Text = p + zamiananaHTML() + k + "&strona=" + index[tabControl1.SelectedIndex];
+                }
+                else
+                {
+                    label2.Text = p + zamiananaHTML() + k ;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void listbox_Click(object sender, EventArgs e)
+        {
+            Form2 f = new Form2(linki[tabControl1.SelectedIndex][listbox[tabControl1.SelectedIndex].SelectedIndex].ToString());
+            f.Show();
+        }
+        private string zamiananaHTML()
+        {
+            string wysz = "";
+            for (int i = 0; i < textBox1.Text.Length; i++)
+            {
+                switch (textBox1.Text[i])
+                {
+                    case ' ': wysz += "%20"; break;
+                    case '!': wysz += "%21"; break;
+                    case '"': wysz += "%22"; break;
+                    case '#': wysz += "%23"; break;
+                    case '$': wysz += "%24"; break;
+                    case '%': wysz += "%25"; break;
+                    case '&': wysz += "%26"; break;
+                    case (char)39: wysz += "%27"; break;
+                    case '(': wysz += "%28"; break;
+                    case ')': wysz += "%29"; break;
+                    case '*': wysz += "%2A"; break;
+                    case '+': wysz += "%2B"; break;
+                    case ',': wysz += "%2C"; break;
+                    case '-': wysz += "%2D"; break;
+                    case '.': wysz += "%2E"; break;
+                    case '/': wysz += "%2F"; break;
+                    case ':': wysz += "%3A"; break;
+                    case ';': wysz += "%3B"; break;
+                    case '<': wysz += "%3C"; break;
+                    case '=': wysz += "%3D"; break;
+                    case '>': wysz += "%3E"; break;
+                    case '?': wysz += "%3F"; break;
+                    case '@': wysz += "%40"; break;
+                    case '[': wysz += "%5B"; break;
+                    case ']': wysz += "%5D"; break;
+                    case '^': wysz += "%5E"; break;
+                    case '_': wysz += "%5F"; break;
+                    case '`': wysz += "%60"; break;
+                    case '{': wysz += "%7B"; break;
+                    case '|': wysz += "%7C"; break;
+                    case '}': wysz += "%7D"; break;
+                    case '~': wysz += "%7E"; break;
+                    case 'Ą': wysz += "%A1"; break;
+                    case 'Ł': wysz += "%A3"; break;
+                    case 'Ś': wysz += "%A6"; break;
+                    case 'Ź': wysz += "%AC"; break;
+                    case 'Ż': wysz += "%AF"; break;
+                    case 'ą': wysz += "%B1"; break;
+                    case 'ł': wysz += "%B3"; break;
+                    case 'ś': wysz += "%B6"; break;
+                    case 'ź': wysz += "%BC"; break;
+                    case 'ż': wysz += "%BF"; break;
+                    case 'Ę': wysz += "%CA"; break;
+                    case 'Ń': wysz += "%D1"; break;
+                    case 'Ó': wysz += "%D3"; break;
+                    case 'ę': wysz += "%EA"; break;
+                    case 'ń': wysz += "%F1"; break;
+                    case 'ó': wysz += "%F3"; break;
+                    default: wysz += textBox1.Text[i]; break;
+                }
+
+            }
+            return wysz;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter sw = new StreamWriter(saveFileDialog1.FileName);
+                foreach (String s in linki[tabControl1.SelectedIndex])
+                {
+                    sw.WriteLine(s);
+                }
+                sw.Close();
+
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            this.tabControl1.Width = this.Width-10;
+            this.tabControl1.Height = this.Height - 141;
+            this.button3.Location = new Point(this.button3.Location.X,this.Height - 86);
+            this.button4.Location = new Point(this.button4.Location.X, this.Height - 86);
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            tabControl1.TabPages.Remove(tabControl1.SelectedTab);
+            indexlist--;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (index[tabControl1.SelectedIndex]-1 >= 0)
+            {
+                index[tabControl1.SelectedIndex]--;
+                if (index[tabControl1.SelectedIndex] ==0)
+                {
+                    Wyszukaniestrony("");
+                }
+                else
+                {
+                    Wyszukaniestrony("&strona=" + index[tabControl1.SelectedIndex].ToString());
+                }
+            }
+            if (index[tabControl1.SelectedIndex] == 0)
+            {
+                button3.Visible = false;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+                    index[tabControl1.SelectedIndex]++;
+                    Wyszukaniestrony("&strona=" + index[tabControl1.SelectedIndex].ToString());
+                    if (index[tabControl1.SelectedIndex] > 0)
+                    {
+                        button3.Visible = true;
+                    }
+         
+        }
+
+      
+
+     
+
+     
+
+       
+    }
+}
---
+++ WindowsFormsApplication1/Form2.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(844, 314);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(844, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(844, 314);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
